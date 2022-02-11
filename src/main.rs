f56ab125d28c7835dab002f1360e78b68f005d44
Joseph Diza <josephm.diza@gmail.com>
2022-02-10 17:22:25 -0800

    Add clap interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{Arg, App, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Optim")
+        .version("0.1.0")
+        .author("Joseph Diza")
+        .about("Lightweight, extremely customizable email client")
+        .arg(Arg::new("v").help("Show verbose output"))
+        .arg(Arg::new("host").help("The email provider"))
+        .arg(Arg::new("username").help("Your email address"))
+        .arg(Arg::new("password").help("Your email password"))
+        .arg(Arg::new("port").help("The port to listen for emails on"))
+        .get_matches();
 }
