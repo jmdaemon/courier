3b343d531670abe12492710c00c5b780a497c944
Joseph Diza <josephm.diza@gmail.com>
2022-03-27 16:42:17 -0700

    Store email settings in Email struct

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use libcourier::cli::init_cli;
 use libcourier::emails::fetch_inbox_top;
 use libcourier::ui::{init_courier, run_app};
 
+#[derive(Default, Debug)]
 pub struct Email {
     host: String,
     username: String,
@@ -27,8 +28,8 @@ impl Email {
     }
 }
 
-pub fn parse_config(emailcfg: Value, key: &str, msg: &str) -> String {
-    emailcfg[key].as_str().expect(msg).to_string()
+pub fn parse_config(emailcfg: &Value, key: &str) -> String {
+    emailcfg[key].as_str().expect(&format!("Could not parse {}", key)).to_string()
 }
 
 fn main() -> Result<()> {
@@ -39,52 +40,43 @@ fn main() -> Result<()> {
 
     let cfg = matches.value_of("emails");
 
-    let config: Value;
     let email: Email;
-
-    let host: &str;
-    let username: &str;
-    let password: &str;
-    let port;
-
     match cfg {
         Some(_) => {
             // Use the credentials found in the config file
             let contents = read_to_string(cfg.unwrap()).expect("Could not parse emails.toml");
-            config = toml::from_str(&contents).unwrap();
+            let config: Value = toml::from_str(&contents).unwrap();
             let emails = &config["emails"];
 
             // For now we'll just pick the only email available
             // which is the first one.
             let emailcfg = &emails[0];
-
-            host = &emailcfg["host"].as_str().expect("Could not parse host.");
-            username = &emailcfg["username"].as_str().expect("Could not parse username.");
-            password = &emailcfg["password"].as_str().expect("Could not parse password.");
-            port = (&emailcfg["port"].as_integer()).unwrap() as u16;
-            email = Email::new_from_str(host, username, password, port);
+            email = Email::new(
+                parse_config(emailcfg, "host"), parse_config(emailcfg, "username"),
+                parse_config(emailcfg, "password"), (&emailcfg["port"].as_integer()).unwrap() as u16);
         }
         None => {
             // Use the credentials passed in as cli parameters
-            host = matches.value_of("host").unwrap_or("imap.gmail.com");
-            username = matches.value_of("username").expect("Username not found.");
-            password = matches.value_of("password").expect("Password not found.");
-            port = matches.value_of("port").unwrap_or("993").parse::<u16>().unwrap();
+            let host = matches.value_of("host").unwrap_or("imap.gmail.com");
+            let username = matches.value_of("username").expect("Username not found.");
+            let password = matches.value_of("password").expect("Password not found.");
+            let port = matches.value_of("port").unwrap_or("993").parse::<u16>().unwrap();
             email = Email::new_from_str(host, username, password, port);
         }
     }
     
-    info!("Hostname: {}", host);
-    info!("Username: {}", username);
-    info!("Password: {}", password);
-    info!("Port: {}", port);
+    debug!("Email: {:?}", email);
+    debug!("Hostname: {}", email.host);
+    debug!("Username: {}", email.username);
+    debug!("Password: {}", email.password);
+    debug!("Port: {}", email.port);
 
     // Initialize and run the app
     let app = init_courier();
     run_app(&app);
 
-    let msg = fetch_inbox_top(host, username, password, port).unwrap().unwrap();
-    println!("{}", msg);
+    let msg = fetch_inbox_top(&email.host, &email.username, &email.password, email.port).unwrap().unwrap();
+    info!("{}", msg);
 
     Ok(())
 }
