c465aa04a64151381c71882af33395ed2aeb1152
Joseph Diza <josephm.diza@gmail.com>
2022-03-27 16:32:11 -0700

    Create Email struct

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,30 @@ use libcourier::cli::init_cli;
 use libcourier::emails::fetch_inbox_top;
 use libcourier::ui::{init_courier, run_app};
 
+pub struct Email {
+    host: String,
+    username: String,
+    password: String,
+    port: u16
+}
+
+impl Email {
+    pub fn new_from_str(host: &str, username: &str, password: &str, port: u16) -> Email {
+        let host = host.to_string(); 
+        let username = username.to_string();
+        let password = password.to_string();
+        Email { host, username, password, port }
+    }
+
+    pub fn new(host: String, username: String, password: String, port: u16) -> Email {
+        Email { host, username, password, port }
+    }
+}
+
+pub fn parse_config(emailcfg: Value, key: &str, msg: &str) -> String {
+    emailcfg[key].as_str().expect(msg).to_string()
+}
+
 fn main() -> Result<()> {
     pretty_env_logger::init();
     let app = init_cli();
@@ -16,6 +40,8 @@ fn main() -> Result<()> {
     let cfg = matches.value_of("emails");
 
     let config: Value;
+    let email: Email;
+
     let host: &str;
     let username: &str;
     let password: &str;
@@ -30,15 +56,13 @@ fn main() -> Result<()> {
 
             // For now we'll just pick the only email available
             // which is the first one.
-            let email = &emails[0];
+            let emailcfg = &emails[0];
 
-            host = &email["host"].as_str().expect("Could not parse host.");
-            username = &email["username"].as_str().expect("Could not parse username.");
-            password = &email["password"].as_str().expect("Could not parse password.");
-            //port = (&emails["port"].to_string()).parse::<u16>().unwrap();
-            //port = (&emails["port"].as_integer()).parse::<u16>().unwrap();
-            port = (&email["port"].as_integer()).unwrap() as u16;
-
+            host = &emailcfg["host"].as_str().expect("Could not parse host.");
+            username = &emailcfg["username"].as_str().expect("Could not parse username.");
+            password = &emailcfg["password"].as_str().expect("Could not parse password.");
+            port = (&emailcfg["port"].as_integer()).unwrap() as u16;
+            email = Email::new_from_str(host, username, password, port);
         }
         None => {
             // Use the credentials passed in as cli parameters
@@ -46,6 +70,7 @@ fn main() -> Result<()> {
             username = matches.value_of("username").expect("Username not found.");
             password = matches.value_of("password").expect("Password not found.");
             port = matches.value_of("port").unwrap_or("993").parse::<u16>().unwrap();
+            email = Email::new_from_str(host, username, password, port);
         }
     }
     
