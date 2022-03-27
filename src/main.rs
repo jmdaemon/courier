01f025ce0c8e103e180f9195ae3486c436076d07
Joseph Diza <josephm.diza@gmail.com>
2022-03-27 12:50:02 -0700

    Read email in user's inbox

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
+extern crate imap;
+extern crate native_tls;
+
 use clap::{Arg, App, AppSettings};
 use log::{debug, error, info, warn};
+
+fn fetch_inbox_top(host: &str, username: &str,
+    password: &str, port: u16) -> imap::error::Result<Option<String>> {
+
+    let tls = native_tls::TlsConnector::builder().build().unwrap();
+    //let client = imap::ClientBuilder::new(host, 993).native_tls()?;
+    let client = imap::connect((host, port), host, &tls).unwrap();
+
+    let mut imap_session = client
+        .login(username, password)
+        .map_err(|e| e.0)?;
+
+    // We want to fetch the first email in the INBOX mailbox
+    imap_session.select("INBOX")?;
+
+    // Fetch message number 1 in this mailbox, along with its RFC822 field.
+    // RFC 822 dictates the format of the body of e-mails
+    let messages = imap_session.fetch("1", "RFC822")?;
+    let message = if let Some(m) = messages.iter().next() {
+        m
+    } else {
+        return Ok(None);
+    };
+
+    // extract the message's body
+    let body = message.body().expect("message did not have a body!");
+    let body = std::str::from_utf8(body)
+        .expect("message was not valid utf-8")
+        .to_string();
+
+    // be nice to the server and log out
+    imap_session.logout()?;
+
+    Ok(Some(body))
+}
 
 fn main() -> Result<(), clap::Error> {
     //env_logger::init();
@@ -16,14 +54,18 @@ fn main() -> Result<(), clap::Error> {
         .arg(Arg::new("port").help("The port to listen for emails on"))
         .get_matches();
     
-    let host = matches.value_of("host").ok_or("Host not found.");
-    let username = matches.value_of("username").ok_or("Username not found.");
-    let password = matches.value_of("password").ok_or("Password not found.");
-    let port = matches.value_of("port").unwrap_or("430");
+    let host = matches.value_of("host").unwrap_or("imap.gmail.com");
+    let username = matches.value_of("username").expect("Username not found.");
+    let password = matches.value_of("password").expect("Password not found.");
+     let port = matches.value_of("port").unwrap_or("993").parse::<u16>().unwrap();
     
-    info!("Hostname: {}", host.unwrap());
-    info!("Username: {}", username.unwrap());
-    info!("Password: {}", password.unwrap());
+    info!("Hostname: {}", host);
+    info!("Username: {}", username);
+    info!("Password: {}", password);
     info!("Port: {}", port);
+
+    let msg = fetch_inbox_top(host, username, password, port).unwrap().unwrap();
+
+    println!("{}", msg);
     Ok(())
 }
